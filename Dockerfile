eaaf2ef10ee65441e25b95c86e1445ba2f472f64
Prakash992 <maneprakash992@gmail.com>
2024-02-02 23:36:14 +0530

    pk

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:78be2ac58aff65cafab561f8be8464e2f3c6b7c5e3009951a7ca1e52c23d9ed0
+size 400
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b5ca106d38c2377784c61326b119c5b37a81137e5f8a2cedf1f8d530ab16c56
+size 2521
